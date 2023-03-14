568b447a8a75668295324cae68f7fb1f20e39cf3
Devving Daniel <dannysykes22@gmail.com>
2023-03-13 22:57:08 -0400

    files that are just about read for turn in

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Secret-Stash
 This application will allow users to generate a password between 8-128 characters long abiding by specific criteria. 
+
+The application is quite simple and to the point there are not many added features but it will get the job done.
+To begin you are asked to stay inside the parameters set for character length and then from there you have the freedom to choose the style of password you would like.
+
+
+
+## Acceptance Criteria
+
+```
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+
--- script.js
+++ script.js
@@ -9,41 +9,50 @@ var symbolOptions = "!@#$%^&*".split("");
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  var storedCharacters= [];
-  var passwordLength = prompt("Please select between 8 - 128 characters for your password!")
+  var storedCharacters= []; // empty array that stores selected character 
+  var generatedText=[]; // empty array that will store the generated text i.e. created password
+
+  var passwordLength = prompt("Please select between 8 - 128 characters for your password!") // initial prompt
+
   if (passwordLength > 128 || passwordLength < 8) {
     alert("The value you choose must stay between 8 - 128 characters.") 
     return ""
-  }
+  } // parameter and alert that informs user they need to stay within provided range
 
   var sym = confirm("Would you like special symbols included within your password ?")
+
     if(sym) {
       storedCharacters = storedCharacters.concat(symbolOptions);
-    }
+    } // if it is confirmed that symbols are needed they will be stored in the storedCharacter array and the concat feature adds them to the other selected styles
     console.log(storedCharacters);
 
     var uppercase = confirm("Would you like UPPERCASE letters included within your password ?")
+
     if(uppercase) {
       storedCharacters = storedCharacters.concat(bigLetters);
     }
     console.log(storedCharacters);
 
     var lowercase = confirm("Would you like lowercase letters included within your password ?")
+
     if(lowercase) {
       storedCharacters = storedCharacters.concat(littleLetters);
     }
     console.log(storedCharacters);
 
     var numbers = confirm("Would you like to include numbers within your password ?")
+
     if(numbers) {
       storedCharacters = storedCharacters.concat(numberOption);
     }
     console.log(storedCharacters);
 
-    if (!bigLetters && !littleLetters && !symbolOptions && !numberOption) {
-      alert("Character type or symbol must be selected to proceed without error")
-      return "Compliance with parameters are for users best interest. Please attempt again!";
+    for (var i = 0; i < passwordLength; i++) {
+      var index = Math.floor(Math.random() * storedCharacters.length);
+      var textAdditions = storedCharacters[index];
+      generatedText.push(textAdditions)
     }
+    return generatedText.join("")
   }
 
 // Write password to the #password input
